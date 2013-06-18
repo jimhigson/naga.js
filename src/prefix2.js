a30d69d6fd3702399f55bfc218e0d44f0d8e62a8
Jim Higson <jim@wikizzle.org>
2013-06-18 18:37:50 +0100

    actually, checking correct number of args is incompatible with currying and currying is better

--- src/prefix2.js
+++ src/prefix2.js
@@ -1,32 +1,18 @@
 define(
-   ['naga/error/halt', 'naga/template', 'naga/validArgs'],
-   function(halt, template, validArgs) {
+   ['naga/error/halt', 'naga/template', 'naga/validArgs', 'naga/curry'],
+   function(halt, template, validArgs, curry) {
 
       var combinerFuncTemplate = template('return a {symbol} b');
       
-      var wrongNumberOfArgumentsForASymbol = halt('expected exactly two arguments for symbol "{symbol}" but got {actualNumber}');
-
       /**
        */
       return function prefix2( symbol ) {
 
-         var wrongNumberOfArgumentsForThisSymbol = wrongNumberOfArgumentsForASymbol(symbol),
-         
-             symbolAsFunction = new Function('a', 'b', combinerFuncTemplate(symbol)); 
+         var symbolAsFunction = new Function('a', 'b', combinerFuncTemplate(symbol)); 
                                     
-         return validArgs(
-                  correctNumberOfArguments(2, wrongNumberOfArgumentsForThisSymbol), 
-                  symbolAsFunction
-         );         
+         return curry( symbolAsFunction );
       };
 
-      function correctNumberOfArguments(n, onFail) {
-        
-         return function(args){
-            args.length == n || onFail(args.length);
-         };      
-      
-      }
    }
    
 );
