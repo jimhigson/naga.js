f172c6e6d58c54a0bc7a8cb8cc33cf0e868bdbe1
Jim Higson <jim@wikizzle.org>
2013-06-18 17:26:07 +0100

    remove eager calculation and dependencies from prefix2 when things go wrong

--- src/prefix2.js
+++ src/prefix2.js
@@ -1,28 +1,33 @@
 define(
-   ['naga/template', 'naga/curry', 'naga/throwError', 'naga/validArgs'],
-   function(template, curry, throwError, validArgs) {
+   ['naga/error/halt', 'naga/template', 'naga/validArgs'],
+   function(halt, template, validArgs) {
 
       var combinerFuncTemplate = template('return a {symbol} b');
-      var wrongNumberOfArguments = throwError(
-         'expected exactly two arguments for symbol {symbol} but got {actualNumber}');
+      
+      var wrongNumberOfArgumentsForASymbol = halt('expected exactly two arguments for symbol "{symbol}" but got {actualNumber}');
 
       /**
        */
       return function prefix2( symbol ) {
 
-         var wrongNumberOfArgumentsForThisSymbol = curry(wrongNumberOfArguments)(symbol),
+         var wrongNumberOfArgumentsForThisSymbol = wrongNumberOfArgumentsForASymbol(symbol),
          
-             symbolAsFunction = new Function('a', 'b', combinerFuncTemplate(symbol)),
-         
-             validateNumberOfArguments = function(args){
-                args.length == 2 || wrongNumberOfArgumentsForThisSymbol(args.length);
-             };
+             symbolAsFunction = new Function('a', 'b', combinerFuncTemplate(symbol)); 
                                     
-         return validArgs(validateNumberOfArguments, symbolAsFunction);         
+         return validArgs(
+                  correctNumberOfArguments(2, wrongNumberOfArgumentsForThisSymbol), 
+                  symbolAsFunction
+         );         
       };
 
+      function correctNumberOfArguments(n, onFail) {
+        
+         return function(args){
+            args.length == n || onFail(args.length);
+         };      
+      
+      }
    }
-   
    
 );
 
