b1044ccb230dd991bb0b437fb880aa0688fc88bc
Jim Higson <jim@wikizzle.org>
2013-06-18 18:44:21 +0100

    new failing test showing bug in compose

--- test/cases/composeTest.js
+++ test/cases/composeTest.js
@@ -1,12 +1,14 @@
 
 DependentTestCase(
-   'naga-chain'
-,  ['naga/compose']
+   'naga-compose'
+,  ['naga/compose', 'naga/add', 'naga/multiply']
 
 ,  {  
-      'test compose works': function(compose) {
+      'test compose works': function(compose, add, multiply) {
       
+         var add3 = compose( add(2), add(1) );
          
+         assertEquals(5, add3(2));
       }
 
    }
